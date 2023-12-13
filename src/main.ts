d6f089ff4ea42bfe44cd96b04d2b1119f00628da
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2023-12-12 22:39:57 -0300

    feat(swagger): create documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+//import * as dotenv from 'dotenv';
+//dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      transform: true,
+      skipNullProperties: true,
+      skipMissingProperties: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Bank Account - API')
+    .setDescription('dev-challenge-bank-account')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT || 3333, async () => {
+    console.log(`Application is running on: ${await app.getUrl()}`);
+  });
 }
+
 bootstrap();
