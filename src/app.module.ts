97b757585293ff8adee8c185207117fde95e3386
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2023-12-13 21:12:23 -0300

    chore: remove hello world endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BankAccountsModule } from './bank-account/bank-account.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BankAccountSchema } from './@core/infra/db/bank-account.schema';
@@ -16,7 +14,5 @@ import { BankAccountSchema } from './@core/infra/db/bank-account.schema';
     }),
     BankAccountsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
